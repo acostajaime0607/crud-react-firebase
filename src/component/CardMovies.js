a0a36e8d19832681a88a1df4c8f02ebe1c00f950
Jaime acosta <acosta.jaime0607@gmail.com>
2022-12-01 22:50:49 -0500

    arreglos para mandar a producion

--- src/component/CardMovies.js
+++ src/component/CardMovies.js
@@ -2,34 +2,53 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function CardMovies({ data, deleteMovies }) {
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
   return (
     <div className="card movie_card">
       <img
-        src="https://picsum.photos/200/300?random=1"
+        src={`https://picsum.photos/200/300?random=${getRandomInt(50)}`}
         className="card-img-top"
         alt="..."
       />
-      <div className="card-body">
-        <Link to={`/editar-movies/${data.id}`}>
-          <button
-            type="button"
-            className="btn btn-warning btn-circle button_1 btn-xl"
-          >
-            <i className="fa-regular fa-pen-to-square"></i>
-          </button>
-        </Link>
-
+      <Link to={`/editar-movies/${data.id}`}>
         <button
           type="button"
-          className="btn btn-danger btn-circle button_2 btn-xl"
-          onClick={() => deleteMovies(data.id)}
+          className="btn btn-warning btn-circle button_1 btn-xl"
         >
-          <i className="fa-solid fa-trash"></i>
+          <i className="fa-regular fa-pen-to-square"></i>
         </button>
-        <h5 className="card-title">{data.titulo}</h5>
-        <span className="movie_info">2019</span>
+      </Link>
+
+      <button
+        type="button"
+        className="btn btn-danger btn-circle button_2 btn-xl"
+        onClick={() => deleteMovies(data.id)}
+      >
+        <i className="fa-solid fa-trash"></i>
+      </button>
+
+      <div className="card-body">
+        <h5
+          className="card-title"
+          style={{ fontSize: "17px", marginTop: "10px" }}
+        >
+          {data.titulo}
+        </h5>
+        <p>{data.resumen}</p>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <span className="movie_info" style={{ marginRight: "3rem" }}>
+            Lenguje : {data.lenguaje}
+          </span>
+          <span className="movie_info">votos: {data.cantidad_votos}</span>
+        </div>
+
+        <span className="movie_info">{data.lanzamiento}</span>
         <span className="movie_info float-right">
-          <i className="fas fa-star"></i> 9 / 10
+          <i className="fas fa-star" style={{ marginLeft: "3.5rem" }}></i>{" "}
+          {data.rating} / 10
         </span>
       </div>
     </div>
